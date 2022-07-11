b01420ae4e6aef6f9db5ef43f457f2a86d517248
minh khue <dominh3214@gmail.com>
2022-07-11 18:15:34 +0700

    first commit

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 
 //rest of the packages
 const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 
 //database
@@ -14,6 +16,7 @@ const connectDB = require('./db/connect')
 
 // routers
 const authRoutes = require('./routes/authRoutes')
+const userRoutes = require('./routes/userRoutes')
 
 //middleware
 const notFoundMiddleware = require('./middleware/not-found')
@@ -21,12 +24,22 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.use(morgan('tiny'))
 app.use(express.json())
+app.use(cookieParser(process.env.JWT_SECRET))
+app.use(express.static('./public'))
+app.use(cors())
 
 app.get('/',(req,res)=>{
   res.send('e-commerce api')
 })
 
+app.get('/api/v1',(req,res)=>{
+  // console.log(req.cookies)
+  console.log(req.signedCookies)
+  res.send('e-commerce api')
+})
+
 app.use('/api/v1/auth',authRoutes)
+app.use('/api/v1/users',userRoutes)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 
 const UserSchema = new mongoose.Schema({
     name:{
@@ -10,6 +13,7 @@ const UserSchema = new mongoose.Schema({
     },
     email:{
     type:String,
+    unique:true,
     required:[true,'Please provide email'],
     validate:{
         validator:validator.isEmail,
@@ -27,5 +31,15 @@ const UserSchema = new mongoose.Schema({
      default:'user',
     },
 });
+
+UserSchema.pre('save',async function(){
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password,salt)
+})
+
+UserSchema.methods.comparePassword = async function(canditatePassword){
+  const isMatch = await bcrypt.compare(canditatePassword,this.password)
+  return isMatch
+}
 
 module.exports = mongoose.model('User', UserSchema)
